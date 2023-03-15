3211f8a929a01406679bb88c42ea56154bd48274
Year <yezhunian@163.com>
2023-03-15 17:07:20 +0800

    更新

--- Src/Client/Assets/Scripts/GameObject/PlayerCamera.cs
+++ Src/Client/Assets/Scripts/GameObject/PlayerCamera.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SkillBridge.Message;
+using Entities;
 
-public class PlayerCamera : MonoBehaviour
+public class PlayerCamera : MonoSingleton<PlayerCamera>
 {
     public GameObject player;
+    public float rotateSpeed = 70.0f;
+    public float rotateMinY = -50f;
+    public float rotateMaxY = 30f;
+
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
 
     private void LateUpdate()
     {
@@ -12,6 +20,27 @@ public class PlayerCamera : MonoBehaviour
             return;
 
         this.transform.position = player.transform.position;
-        // this.transform.rotation = player.transform.rotation;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if (mouseX != 0 || mouseY != 0)
+        {
+            xRotation -= mouseX * rotateSpeed * 0.02f;
+            yRotation += mouseY * rotateSpeed * 0.02f;
+            yRotation = ClampValue(yRotation, rotateMinY, rotateMaxY);
+
+            Quaternion rotation = Quaternion.Euler(-yRotation, -xRotation, 0);
+            this.transform.rotation = rotation;
+        }
+    }
+
+    float ClampValue(float value, float min, float max)
+    {
+        if (value < -360)
+            value += 360;
+        if (value > 360)
+            value -= 360;
+        return Mathf.Clamp(value, min, max);
     }
 }
